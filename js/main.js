e3a73359e61b09e905bb976582db65a4fa97b1ee
1Hizaki3 <biscoitomuleque@gmail.com>
2026-08-13 13:00:41 +0000

    aula3

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const canvas = document.getElementById("game")
+const ctx = canvas.getContext("2d");
+
+canvas.width = 900;
+canvas.height = 600;
+
+const keys = {};[
+
+    window.addEventListener("keydown",(e)=>{
+
+        keys[e.key.toLowerCase()] = true;
+
+    });
+
+    window.addEventListener("keyup",(e)=>{
+
+        keys[e.key.toLowerCase()] = false;
+
+    });
+
+    const player = new Player(
+        canvas.width/2
+        canvas.height/2
+    );
+
+    )
+    })
+    })
+]
